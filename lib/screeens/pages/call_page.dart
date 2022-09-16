f09db06b37ca953ed3e0cbeb30a4cbd1afde6ca4
chambaga256 <chambagaabudlah@gmail.com>
2022-09-16 13:59:34 +0300

    first

---
+++ lib/screeens/pages/call_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Call_Page extends StatelessWidget {
+  const Call_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('callpage'),
+    );
+  }
+}
---
+++ lib/screeens/pages/contact_page.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+
+class Contact_Page extends StatelessWidget {
+  const Contact_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('contact'),
+    );
+  }
+}
